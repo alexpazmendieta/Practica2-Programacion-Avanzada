26e2f9e89b28ef58ee92434d45d20061c34331f2
Alejandro Paz <alexpazmendieta@hotmail.com>
2024-07-14 11:54:39 -0600

    Creación del README

--- Practica 2-1.py
+++ Practica 2-1.py
@@ -1,18 +1,14 @@
 import numpy as np
 
-# Establecer las dimensiones del array
 dimensiones = (5, 4, 3)
 
-# Crear el array tridimensional con valores aleatorios entre 0 y 100
 array_tridimensional = np.random.randint(0, 101, size=dimensiones)
 
-# Inicializar variables para el valor mínimo y máximo y sus posiciones
 min_valor = float('inf')
 max_valor = float('-inf')
 pos_min = None
 pos_max = None
 
-# Recorrer el array tridimensional
 for i in range(dimensiones[0]):
     for j in range(dimensiones[1]):
         for k in range(dimensiones[2]):
@@ -23,8 +19,7 @@ for i in range(dimensiones[0]):
             if valor > max_valor:
                 max_valor = valor
                 pos_max = (i, j, k)
-
-# Mostrar los resultados
+                
 print("Array tridimensional:")
 print(array_tridimensional)
 print(f"Elemento más pequeño: {min_valor} en la posición {pos_min}")
---
+++ README.md
@@ -0,0 +1,4 @@
+Para el primer programa se utilizaron los conceptos vistos en clase para realizar el ejercio solicitado de crear un arreglo tridimensional  de 5x4x3 que pueda encontrar la ubicación de los valores mayores y menores dentro de ella.
+
+En el segundo caso se tuvo que investigar como generar la transpuesta de un plano en Python y utilizar el proceso para resolver el ejercicio.
+
